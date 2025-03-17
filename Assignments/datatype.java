0e7add563bcaa68ef481c2fb76771f9a66594a7c
Shankarshana94 <shankarshana94@gmail.com>
2025-03-17 22:07:47 +0530

    Add files via upload
    Assignment 1

---
+++ Assignments/datatype.java
@@ -0,0 +1,34 @@
+package program1;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		
+		byte age=30;
+		System.out.println(age);
+		
+		int ageNumber =3232;
+			System.out.println(ageNumber);
+			
+		long num = 24242;
+		System.out.println(num);
+		
+		float Decimal=23.2F;
+	   System.out.println(Decimal);
+
+	   double rate=24242424.242D;
+	   System.out.println(rate);
+	   
+	   char letter='F';
+	   System.out.println(letter);
+	   
+	   boolean choose=true;
+	   System.out.println(choose);
+	   
+	   String name="shankarshana";
+	   System.out.println(name+age+letter+num+Decimal+rate+ageNumber);
+	   
+	   
+	}
+
+}
